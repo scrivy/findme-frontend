28e986c5e08937ad87b5482767ce40ce29ca9e82
scrivy <scrivinator@gmail.com>
2015-03-28 19:19:15 -0700

    converted to angular

--- public/app.js
+++ public/app.js
@@ -1,11 +1,112 @@
 'use strict';
 
-angular.module('findme', ['ngNewRouter', 'findme.map'])
-    .controller('AppController', AppController)
+angular.module('findme',[
+        'ngRoute',
+        'ngCookies',
+        'findme.map'
+    ]).
+
+    config(['$routeProvider', configRoutes]).
+
+    factory('geoLocate', [geoLocateService]).
+    factory('ws', [wsService])
 ;
 
-function AppController($router) {
-    $router.config([
-        {path: '/', component: 'map'}
-    ]);
+function configRoutes ($routeProvider) {
+    $routeProvider.
+        when('/', {
+            templateUrl: 'components/map/map.html',
+            controller: 'mapController'
+        })
+    ;
+}
+
+function geoLocateService() {
+    var onSuccessFns = [];
+
+    window.navigator.geolocation.watchPosition(geo_success, geo_error, {enableHighAccuracy: true});
+
+    function geo_success(position) {
+        onSuccessFns.
+            forEach(function(fn) {
+                fn(position);
+            })
+        ;
+    }
+
+    function geo_error() {
+    	console.log('geolocation error');
+    }
+
+    return {
+        onSuccess: function(fn) {
+            onSuccessFns.push(fn);
+        }
+    };
+}
+
+function wsService() {
+    var ws = new WebSocket('ws://' + window.location.host + '/ws'),
+        handlers = {};
+
+    ws.onopen = function() {
+        console.log('webSocket opened');
+    };
+
+    ws.onmessage = function(event) {
+        try {
+            var message = JSON.parse(event.data);
+        } catch(e) {
+            console.error(e.message);
+            console.error('raw message: ');
+            console.error(event);
+            return;
+        }
+        console.log('webSocket message: ', message);
+
+        if (!message.action || !message.data) return console.error('ws: no action or data property from incoming message');
+
+        if (handlers[message.action]) {
+            handlers[message.action].forEach(function(fn) {
+                fn(message.data);
+            });
+        } else {
+            console.error('ws: no handlers for action: ' + message.action);
+        }
+
+    };
+
+    ws.onerror = ws.onclose = function() {
+        console.error('websocket something bad happened with the connection');
+    };
+
+    return {
+
+        on: function(name, fn) {
+            if (!name || !fn) return console.error("ws.on('name', function(data) {}): something's missing...");
+
+            if (!handlers[name]) {
+                handlers[name] = [];
+            }
+
+            handlers[name].push(fn);
+        },
+
+        send: function(action, data) {
+            if (ws.readyState === 1) {
+                try {
+                    ws.send(JSON.stringify({ action: action, data: data}));
+                } catch(e) {
+                    console.log('caught exception', e);
+                };
+
+            } else {
+                console.error('ws sockets screwy');
+            }
+        },
+
+        _ws: ws
+
+    };
+
 }
--- public/components/map/map.js
+++ public/components/map/map.js
@@ -1,7 +1,138 @@
 'use strict';
 
-angular.module('findme.map', [])
-    .controller('MapController', [function() {
-        this.blab = 'oaeu';
-    }])
+angular.module('findme.map', []).
+
+    controller('mapController', ['$scope', '$cookies', 'ws', 'geoLocate', mapController]).
+
+    directive('map', mapDirective)
+
 ;
+
+function mapController($scope, $cookies, ws, geoLocate) {
+    var everyone = {};
+
+    ws.on('allLocations', function(data) {
+        console.log(data);
+
+        var locations = data.locations;
+
+        Object.keys(locations).
+            forEach(function(id) {
+                everyone[id] = {
+                    marker: L.marker(locations[id].latlng).addTo($scope.map),
+                    circle: L.circle(locations[id].latlng, locations[id].accuracy).addTo($scope.map),
+                    line: L.polyline([$scope.my.marker.getLatLng(), locations[id].latlng]).addTo($scope.map),
+                    trail: L.polyline([locations[id].latlng]).addTo($scope.map)
+                };
+            })
+        ;
+
+        $cookies.put('id', data.id);
+    });
+
+    ws.on('updateLocation', function(location) {
+
+        if (everyone[location.id]) {
+            var thisGuy = everyone[location.id];
+
+            thisGuy.marker.
+                setLatLng(location.latlng).
+                setOpacity(1);
+
+            thisGuy.circle.
+                setLatLng(location.latlng).
+                setRadius(location.accuracy).
+                setStyle({opacity: 0.5});
+
+            thisGuy.line.
+                setLatLngs([
+                    $scope.my.marker.getLatLng(),
+                    location.latlng
+                ]);
+
+            thisGuy.trail.
+                addLatLng(location.latlng);
+
+        } else {
+            everyone[location.id] = {
+                marker: L.marker(location.latlng).addTo($scope.map),
+                circle: L.circle(location.latlng, location.accuracy).addTo($scope.map),
+                line: L.polyline([$scope.my.marker.getLatLng(), location.latlng]).addTo($scope.map),
+                trail: L.polyline([location.latlng]).addTo($scope.map)
+            };
+        }
+
+    });
+
+    geoLocate.onSuccess(function(position) {
+        console.log(position);
+
+        if (ws._ws && ws._ws.readyState) {
+            console.log('got a fix');
+
+            var data = {
+                latlng: [position.coords.latitude, position.coords.longitude],
+                accuracy: Math.ceil(position.coords.accuracy)
+            };
+
+            ws.send('updateLocation', data);
+
+            $scope.my.marker.setLatLng(data.latlng);
+            $scope.my.circle.
+                setLatLng(data.latlng).
+                setRadius(position.coords.accuracy)
+            ;
+
+            Object.keys(everyone).
+              forEach(function(id) {
+                everyone[id].line.
+                  setLatLngs([
+                    data.latlng,
+                    everyone[id].marker.getLatLng()
+                  ])
+              })
+            ;
+        }
+
+    });
+
+    // old jquery code, will convert later
+    // modal
+    var $modalDiv = $('.ui.modal');
+    $('#settings').click(function() {
+        $modalDiv.modal('show');
+    });
+
+    var fixed = false;
+    $('.ui.button').click(function() {
+        $(this).toggleClass('active');
+        fixed = !fixed;
+        ws.send('changeFixedLocationState', fixed);
+    });
+
+}
+
+function mapDirective() {
+    return {
+        restrict: 'E',
+        link: function(scope, element) {
+            scope.map = L.map(element[0]).setView([37.76, -122.44], 11);
+
+            L.tileLayer('/tiles/{z}/{x}/{y}.png').addTo(scope.map);
+
+            scope.my = {
+                marker: L.marker([0, 0], {
+                    icon: L.icon({
+                        iconUrl: 'images/mymarker.png',
+                        iconSize: [25, 41],
+                        iconAnchor: [12, 40]
+                    }),
+                    alt: "Me!"
+                }).addTo(scope.map),
+                circle: L.circle([0, 0], 50, {
+                    fillOpacity: 0.5
+                }).addTo(scope.map)
+            };
+        }
+    };
+}
